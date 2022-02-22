3f6c71ed80d1e2d4366322ad3d131fde44fe0391
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-02-21 17:46:02 -0800

    Update cartoon_transformation.py

--- cartoon_transformation.py
+++ cartoon_transformation.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.write("Hello world. This is Phuoc Nguyen")
+import cv2
+import numpy as np
+from upload_image import upload_image
+
+st.title("Real Image To Cartoon Image Transformation")
+
+original_img = upload_image(isShown=True)
+
+algo_option = st.selectbox(
+    label='What algorithm would you like to choose?',
+    options=('Median',
+             'Gaussian',
+             'Bilateral')
+)
