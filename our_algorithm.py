7e32d27570b71c2198255ce62d5d4bae62c758d7
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2022-03-10 16:24:44 -0800

    solve conflict

--- our_algorithm.py
+++ our_algorithm.py
@@ -37,12 +37,12 @@ def portrait_pictures(I):
     start = timeit.default_timer()
     k = 5
     for i in range(5):
-        I = bilateral_filter(I, k, k/100, k)
+        I = bilateral_filter(I, k, k / 100, k)
 
     ip_gray = convert_color_space_RGB_to_GRAY(I)
-    
+
     c = canny_edge_detection(ip_gray)
-    I = I - increase_dim(c,I)*0.1
+    I = I - increase_dim(c, I) * 0.1
     I = I.clip(0.0, 255.0).astype(np.uint8)
 
     stop = timeit.default_timer()
@@ -50,23 +50,3 @@ def portrait_pictures(I):
 
     return I
 
-if __name__ == '__main__':
-
-    I = cv2.imread('./img_test/test3.jpg',
-                cv2.COLOR_BGR2RGB).astype('float32')  
-
-    
-    I = portrait_pictures(I)
-    
-
-    cv2.imwrite('./img_test/test3_result.jpg', I)
-
-
-    # I = cv2.imread('./img_test/psu1.jpg',
-    #             cv2.COLOR_BGR2RGB).astype('float32')  
-
-    
-    # I = landscape_pictures(I)
-    
-
-    # cv2.imwrite('./img_test/psu1_result.jpg', I)   
